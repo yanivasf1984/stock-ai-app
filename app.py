2404bf9b99d3f80cf53598b4b3fb5b1d36f0e1c5
yanivasf1984 <yanivasf@gmail.com>
2026-09-23 10:05:50 +0300

    Update app.py

--- app.py
+++ app.py
@@ -267,8 +267,8 @@ if st.sidebar.button("🗑️ הסר מהרשימה") and remove_ticker != "-- �
 
 st.sidebar.markdown("---")
 st.sidebar.header("📱 הגדרות בוט טלגרם (אופציונלי)")
-tg_token = st.sidebar.text_input("Telegram Bot Token:", type="password")
-tg_chat_id = st.sidebar.text_input("Telegram Chat ID:")
+tg_token = st.sidebar.text_input("Telegram Bot Token:", type="8979601396:AAFQJlLDf81HJPh8RjkpcpzQYxYAAHd8jpw")
+tg_chat_id = st.sidebar.text_input("Telegram Chat ID:5117812191")
 
 # ---------------------------------------------------------
 # מצב 1: ניתוח מניה בודדת
